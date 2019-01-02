3e692ffbe81788421ff5e543698af686c11c0270
xaviguasch <xaviguasch@gmail.com>
2019-01-02 19:22:18 +0100

    making api requests through request npm package

---
+++ first-request.js
@@ -0,0 +1,6 @@
+const request = require('request');
+request('https://teamtreehouse.com/xaviguasch.json', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
